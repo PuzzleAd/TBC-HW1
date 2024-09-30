44807131c0dc1436aaea8b3db05f9e5239e72930
PuzzleAd <mamulashviliirakli65@gmail.com>
2024-09-30 19:14:38 +0400

    Adding contact component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Content from "./Main/Content";
 import Footer from "./Footer/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./About/About";
+import Contact from "./Contact/Contact";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Content />} />
           <Route path="/about" element={<About/>} />
-          <Route path="/" element={<Content />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/Contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <section className="contact-content">
+      <h1>კონტაქტი</h1>
+      <p>
+        ნებისმიერი შეკითხვის შემთხვევაში დაგვიკავშირდით, სიამოვნებით გიპასუხებთ:
+        <br />
+        Email: support@example.com
+        <br />
+        Phone: (123) 456-7890
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
