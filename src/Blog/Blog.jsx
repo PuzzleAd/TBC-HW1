c0fc1ec5907896a6a4e1e4133afe7c7a5ee0d975
PuzzleAd <mamulashviliirakli65@gmail.com>
2024-10-03 20:35:07 +0400

    Adding blog page content

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./Blog.css"
+import datas from '../data'
 
 const Blog = () => {
   return (
-    <div>
-      
+    <div className='blog-content'>
+        {datas.map((data) => (
+            <div className="blog-box">
+                <img src={data.image} alt="#" />
+                <div className="blog-text">
+                    <h1>{data.header}</h1>
+                    <p>{data.text}</p>
+                    <a href="#">იხილეთ დეტალები</a>
+                </div>
+
+            </div>
+        ))}
     </div>
   )
 }
